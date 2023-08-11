1195328d1b0fa3057cf28ff37e7be2b69e98b04f
Oliver Kurmis <oliver@kurmis.com>
2023-08-11 13:21:52 +0200

    Backport "Refactor/fix implement signature image process"
    https://github.com/lsnepomuceno/laravel-a1-pdf-sign/pull/85/

--- src/SignaturePdf.php
+++ src/SignaturePdf.php
@@ -263,7 +263,9 @@ class SignaturePdf
             $this->pdf->AddPage($width > $height ? 'L' : 'P', [$width, $height]);
             $this->pdf->useTemplate($pageIndex);
 
-            if ($this->hasSealImgOnEveryPages) $this->implementSignatureImage((int) $pageIndex);
+            if ($this->hasSealImgOnEveryPages || $i === ($this->image['page'] ?? 0)) {
+                $this->implementSignatureImage($i);
+            }
         }
 
         $certificate = $this->cert->getCert()->original;
@@ -279,7 +281,6 @@ class SignaturePdf
             'A' // Authorize certificate
         );
 
-        if (!$this->hasSealImgOnEveryPages) $this->implementSignatureImage();
         if (empty($this->fileName)) $this->fileName = Str::orderedUuid();
         if ($this->hasSignedSuffix) $this->fileName .= '_signed';
 
